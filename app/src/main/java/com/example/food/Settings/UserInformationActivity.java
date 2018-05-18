0ea8add134cac5e33f7590d76d1daec411682815
java00801 <taipeitex00801@gmail.com>
2018-05-18 19:07:54 +0800

    0518 新增修改個人資料

--- app/src/main/java/com/example/food/Settings/UserInformationActivity.java
+++ app/src/main/java/com/example/food/Settings/UserInformationActivity.java
@@ -1,21 +1,132 @@
 package com.example.food.Settings;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.food.R;
 
+import java.util.Calendar;
 import java.util.Objects;
 
-public class UserInformationActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserInformationActivity extends AppCompatActivity implements
+        DatePickerDialog.OnDateSetListener {
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
-
+        showNow();
         initContent();
+        selectCardView();
+    }
+
+    private void selectCardView() {
+        /* 個人頭像 */
+        CircleImageView cvUserImage = findViewById(R.id.cvUserImage);
+        cvUserImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        /* 密碼 */
+        CardView cvUserPassword = findViewById(R.id.userPassword);
+        cvUserPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(UserInformationActivity.this)
+                        .title(R.string.textNickname)
+                        .inputType(InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                        .widgetColorRes(R.color.colorBody)
+                        .input(0, 0,  new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+
+                            }
+                        }).show();
+            }
+        });
+        /* 暱稱 */
+        CardView cvUserNickname = findViewById(R.id.userNickname);
+        cvUserNickname.setOnClickListener(new View.OnClickListener() {
+            TextView tvUserNickname = findViewById(R.id.tvUserNickname);
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(UserInformationActivity.this)
+                        .title(R.string.textNickname)
+                        .inputType(InputType.TYPE_CLASS_TEXT)
+                        .widgetColorRes(R.color.colorBody)
+                        .input(0, 0,  new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+                                tvUserNickname.setText(input);
+                            }
+                        }).show();
+            }
+        });
+        /* 生日 */
+        CardView cvUserBirthday = findViewById(R.id.userBirthday);
+        cvUserBirthday.setOnClickListener(new View.OnClickListener() {
+            TextView tvUserNickname = findViewById(R.id.tvUserNickname);
+            @Override
+            public void onClick(View view) {
+                DatePickerDialogFragment datePickerFragment = new DatePickerDialogFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                datePickerFragment.show(fm, "datePicker");
+            }
+        });
+        /* 性別 */
+        CardView cvUserGender = findViewById(R.id.userGender);
+        cvUserGender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(UserInformationActivity.this)
+                        .title(R.string.text_gender)
+                        .items(R.array.genderArray)
+                        .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                                switch (which) {
+                                    case 0:
+                                        Toast.makeText(UserInformationActivity.this,"不顯示",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 1:
+                                        Toast.makeText(UserInformationActivity.this,"男",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 2:
+                                        Toast.makeText(UserInformationActivity.this,"女",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    default:
+                                        dialog.cancel();
+                                        break;
+                                }
+                                return true;
+                            }
+                        })
+                        .positiveText(R.string.text_btYes)
+                        .show();
+            }
+        });
+
     }
 
     private void initContent() {
@@ -24,5 +135,28 @@ public class UserInformationActivity extends AppCompatActivity {
 
         setSupportActionBar(settingsUserInformationToolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void showNow() {
+        Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+    /* 選擇日期的跳脫視窗 */
+    public static class DatePickerDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            UserInformationActivity uiActivity = (UserInformationActivity) getActivity();
+            return new DatePickerDialog(
+                    uiActivity, uiActivity, uiActivity.mYear, uiActivity.mMonth, uiActivity.mDay);
+        }
+    }
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        mYear = year;
+        mMonth = month;
+        mDay = day;
     }
 }
