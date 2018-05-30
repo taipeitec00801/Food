819bbc44b0280e3cc7f0ba7ff6c326f78f037312
yen23 <iammissnacy7@gmail.com>
2018-05-30 20:13:27 +0800

    5/30Login

---
+++ app/src/main/java/com/example/food/Member/Member2Activity.java
@@ -0,0 +1,86 @@
+package com.example.food.Member;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.food.DAO.Member;
+import com.example.food.Main.MainActivity;
+import com.example.food.R;
+
+import static com.example.food.R.drawable.btn_bg_set;
+
+
+public class Member2Activity extends AppCompatActivity {
+    private  Button btConfirm, btLike1, btLike2, btLike3, btLike4, btLike5;
+    private  Button btLike6, btLike7, btLike8, btLike9, btLike10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered2);
+
+        findViews();
+        click(btLike1);
+        click(btLike2);
+        click(btLike3);
+        click(btLike4);
+        click(btLike5);
+        click(btLike6);
+        click(btLike7);
+        click(btLike8);
+        click(btLike9);
+        click(btLike10);
+
+
+
+
+
+    }
+
+    private void findViews() {
+        btConfirm = findViewById(R.id.btNext);
+        btLike1 = findViewById(R.id.btLike1);
+        btLike2 = findViewById(R.id.btLike2);
+        btLike3 = findViewById(R.id.btLike3);
+        btLike4 = findViewById(R.id.btLike4);
+        btLike5 = findViewById(R.id.btLike5);
+        btLike6 = findViewById(R.id.btLike6);
+        btLike7 = findViewById(R.id.btLike7);
+        btLike8 = findViewById(R.id.btLike8);
+        btLike9 = findViewById(R.id.btLike9);
+        btLike10 = findViewById(R.id.btLike10);
+    }
+
+//    public void onClick(Button button) {
+//        button.setBackgroundResource(R.drawable.btn_bg_set);
+//    }
+//
+    public void click(Button button) {
+
+        button.setOnClickListener(new View.OnClickListener() {
+            private boolean i = true;
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                if(i == true){
+                    v.setBackgroundResource(R.drawable.ripple_sample);
+//                    v.setTextDirection(R.);
+//                    v.setBackgroundColor(R.color.lgColorText);
+                    i = false;
+                }else{
+                    v.setBackgroundResource(R.drawable.btn_bg);
+                    i = true;
+                }
+
+            }
+        });
+        //demo
+
+    }
+
+}
--- app/src/main/java/com/example/food/Member/MemberActivity.java
+++ app/src/main/java/com/example/food/Member/MemberActivity.java
@@ -1,25 +1,19 @@
 package com.example.food.Member;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.example.food.R;
 
@@ -29,7 +23,7 @@ public class MemberActivity extends AppCompatActivity implements
         DatePickerDialog.OnDateSetListener {
     private EditText etUser , etPassword, etBirthday, etGender;
     private de.hdodenhof.circleimageview.CircleImageView cvImage;
-    private Button btConfirm;
+    private Button btNext;
     private int mYear, mMonth, mDay;
     private Spinner spGender;
     MemderBeanActivity mb = new MemderBeanActivity();
@@ -37,17 +31,17 @@ public class MemberActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered);
-        btConfirm = findViewById(R.id.btConfirm);
+        btNext = findViewById(R.id.btNext);
         findViews();
         showNow();
         showSpinner();
 //        getDropDownView();
 //        initContent();
-        btConfirm.setOnClickListener(new Button.OnClickListener() {
+        btNext.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MemberActivity.this, MemberActivity.class);
+                intent.setClass(MemberActivity.this, Member2Activity.class);
                 startActivity(intent);
 //                finish();
 
